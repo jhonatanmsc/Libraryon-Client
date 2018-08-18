a7b5e6559f8fb383578721e60fd64ee573cdac80
Akijunior <suitsu19@gmail.com>
2018-08-18 16:42:48 -0300

    Envio da página de registro de usuário (erro post)

--- src/main.js
+++ src/main.js
@@ -6,86 +6,91 @@ import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 
+
+import VueResource from "vue-resource"
+Vue.use(VueResource);
+
+
 Vue.config.productionTip = false
+
 Vue.use(Vuex);
 //		Setando regras do login com token JWT
 const store = new Vuex.Store({
-  state: {
-    jwt: localStorage.getItem('token_access'),
-    jwt_refresh: localStorage.getItem('token_refresh'),
-    endpoints: {
-      obtainJWT: 'http://localhost/api/token/jwt/',
-      refreshJWT: 'http://localhost/api/token/jwt/refresh/',
-    }
-  },
-  mutations: {
-    updateToken(state, newToken) {
-      localStorage.setItem('token', newToken);
-      state.jwt = newToken;
-    },
-    removeToken(state) {
-      localStorage.removeItem('token');
-      state.jwt = null;
-      state.jwt_refresh = null;
-    }
-  },
-  actions: {
-    obtainToken(user) {
-      const payload = user;
-
-      http.post(this.state.endpoints.obtainJWT, payload)
-        .then(response => {
-          let acessToken = response.data.access
-          let refreshToken = response.data.refresh
-          localStorage.setItem('token_access', acessToken)
-          localStorage.setItem('token_refresh', refreshToken)
-          this.commit('updateToken', response.data.token);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-    },
-    refreshToken() {
-      const payload = {
-        token: this.state.jwt
-      }
-
-      http.post(this.state.endpoints.refreshJWT, payload)
-        .then((response) => {
-          this.commit('updateToken', response.data.token)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
-    inspectToken() {
-      const token = this.state.jwt;
-      if (token) {
-        const decoded = jwt_decode(token);
-        const exp = decoded.exp
-        // SE está expirando em 30 min (1800 segs) E não está atingindo sua vida útil (24hrs - 30 min)
-        // => REFRESH
-        // SE está expirando em 30 min E está atingindo sua vida útil 
-        // => NÃO ATUALIZAR 
-        if (exp - (Date.now()/1000) < 1800) {
-          this.dispatch('refreshToken')
-          return true;
-        } else if (!(exp - (Date.now() / 1000) < 1800)) {
-          return false;
+    state: {
+        jwt: localStorage.getItem('token_access'),
+        jwt_refresh: localStorage.getItem('token_refresh'),
+        endpoints: {
+            obtainJWT: 'http://localhost/api/token/jwt/',
+            refreshJWT: 'http://localhost/api/token/jwt/refresh/',
         }
-      } else {
-        console.log('TOKEN NOT OK');
-      }
+    },
+    mutations: {
+        updateToken(state, newToken) {
+            localStorage.setItem('token', newToken);
+            state.jwt = newToken;
+        },
+        removeToken(state) {
+            localStorage.removeItem('token');
+            state.jwt = null;
+            state.jwt_refresh = null;
+        }
+    },
+    actions: {
+        obtainToken(user) {
+            const payload = user;
+
+            http.post(this.state.endpoints.obtainJWT, payload)
+                .then(response => {
+                    let acessToken = response.data.access
+                    let refreshToken = response.data.refresh
+                    localStorage.setItem('token_access', acessToken)
+                    localStorage.setItem('token_refresh', refreshToken)
+                    this.commit('updateToken', response.data.token);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        refreshToken() {
+            const payload = {
+                token: this.state.jwt
+            }
+
+            http.post(this.state.endpoints.refreshJWT, payload)
+                .then((response) => {
+                    this.commit('updateToken', response.data.token)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
+        inspectToken() {
+            const token = this.state.jwt;
+            if (token) {
+                const decoded = jwt_decode(token);
+                const exp = decoded.exp
+                    // SE está expirando em 30 min (1800 segs) E não está atingindo sua vida útil (24hrs - 30 min)
+                    // => REFRESH
+                    // SE está expirando em 30 min E está atingindo sua vida útil 
+                    // => NÃO ATUALIZAR 
+                if (exp - (Date.now() / 1000) < 1800) {
+                    this.dispatch('refreshToken')
+                    return true;
+                } else if (!(exp - (Date.now() / 1000) < 1800)) {
+                    return false;
+                }
+            } else {
+                console.log('TOKEN NOT OK');
+            }
+        }
     }
-  }
 });
 
 
 
 new Vue({
-  el: '#app',
-  router,
-  store: store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store: store,
+    render: h => h(App)
 });
-
--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,18 @@ import Resource from 'vue-resource'
 import Home from '@/components/home'
 import Login from '@/components/login'
 import Autores from '@/components/autores'
+import Register from '@/components/register'
 
 Vue.use(Router)
 Vue.use(Resource)
 
 export default new Router({
-  mode: 'history',
-  base: __dirname,
-  routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/login', name: 'login', component: Login},
-    { path: '/autores', name: 'autores', component: Autores },
-  ]
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        { path: '/', name: 'home', component: Home },
+        { path: '/login', name: 'login', component: Login },
+        { path: '/autores', name: 'autores', component: Autores },
+        { path: '/novo-leitor', name: 'novo-leitor', component: Register },
+    ]
 })
